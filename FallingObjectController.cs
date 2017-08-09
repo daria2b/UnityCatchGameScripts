020442b35aa4fcceb58ce9f46792de0f75659ca0
Daria B <d.bagina@gmail.com>
2017-08-09 11:40:32 -0400

    Removed unnecessary code

--- FallingObjectController.cs
+++ FallingObjectController.cs
@@ -10,22 +10,6 @@ public class FallingObjectController : MonoBehaviour {
 	public int coinsValue = 0;
 	public float damageValue = 0f;
 
-	//reference to the script attached to the player
-	//public Score scoreScript;
-
-	//new Static variable to hold current score
-	//private static int currentScore;  
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//currentScore = Score.score;
-	}
-
 	//Recalculate the player score according to the value assigned to this falling object
 	//Then destroy self on contact with other colliders (to be carried over from the destroyer colliders)
 	void OnTriggerEnter2D (Collider2D other) {
