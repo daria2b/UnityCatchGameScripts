f50c6a12ff0cb42ea3d2dabccf9afefd05b2f504
Daria B <d.bagina@gmail.com>
2017-08-09 12:10:42 -0400

    Added groundwork for Jump and Speed evolutions
    Remember to put correct tag to the ground object for the script to correctly update the touchingGround boolean.
    Added booleans to control which evolution the player chose.
    Need to check if jumping script actually works. 
    Evolutions will have to be changed by the script controlling the Hub, most likely.
    Consider if need to DontDestroyOnLoad in Awake ().

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 //this script needs to communicate with GameController script as the controls need to be turned off when the game hasn't started yet
 public class PlayerController : MonoBehaviour {
 
-	public float maxSpeed = 5f;
+	public float maxSpeed = 8f;
+	public float jumpForce = 10f;
 	// public float rotationSpeed = 0.1f;	// Rotation is not being used here, the CircleCollider makes the object rotate
 	//references to different components of this game object
 	Rigidbody2D myRB;
@@ -13,6 +14,10 @@ public class PlayerController : MonoBehaviour {
 	//boolean variables will be used to control player rotation and what side it faces
 	bool facingRight;
 	bool facingUp;
+	//boolean to allow or disallow jumping
+	bool touchingGround;
+	//controls player evolutions
+	bool jump, speed;
 
 	//boolean to check if we can control the player (the game started or not)
 	private bool canControl;
@@ -26,7 +31,14 @@ public class PlayerController : MonoBehaviour {
 		myAnimation = GetComponent<Animator> ();	
 		facingRight = true;
 		canControl = true;
+		touchingGround = false;
+		//the player has not acquired any evolutions at the start of the game, therefore, can only roll to left and right
+		jump, speed = false;
 		startPosition = transform.position;
+		//as soon as the player evolved to jump
+		if (speed) {
+			maxSpeed = 12f;
+		}
 	}
 	
 	// Update is called once per frame
@@ -38,9 +50,6 @@ public class PlayerController : MonoBehaviour {
 			//interract with the animator if buttom was pressed, so movement speed is not 0
 			myAnimation.SetFloat ("speed", Mathf.Abs (move));	
 
-			//if (move != 0)	// Rotation is not being used in this case, the CircleCollider makes the object rotate
-			//	transform.Rotate (Vector3.forward * move * rotationSpeed);
-
 			//move the player by using velocity (not physics)
 			myRB.velocity = new Vector2 (move * maxSpeed, myRB.velocity.y);
 
@@ -50,13 +59,31 @@ public class PlayerController : MonoBehaviour {
 			else if (move < 0 && facingRight)
 				Flip ();
 
-			//reset rotation to 0 if not moving so that the idle jump animation was displayed correctly
+			//reset rotation to 0 if not moving so that the idle jump animation is displayed correctly
 			if (move == 0) {
 				transform.rotation = Quaternion.identity;
 			}
+			
+			//as soon as the player evolved to jump
+			if (jump) {
+				if(touchingGround && Input.GetKeyDown (KeyCode.Space)){
+					touchingGround = false;
+					//need to double check if this works
+					rigidbody2D.AddForce (new Vector2 (0,jumpForce));
+				}
+			}
+			
 		} 
 	}
-
+	
+	//reset the player touchingGround booleand as soon as encounters the ground object
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "Ground") {
+			touchingGround = true;
+		} 
+	}
+	
+	//flip sprite to the side the player is facing
 	void Flip () {
 		facingRight = !facingRight;
 		Vector3 theScale = transform.localScale;
