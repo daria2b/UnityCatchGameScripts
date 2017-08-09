96570536ebd4d8f94c0b7e8aacaf340d16cbdd2d
Daria2b <d.bagina@gmail.com>
2017-08-08 20:31:20 -0400

    Evolve script added
    Changes made to the SafeZoneController to make evolve buttons work. A separate script also added, but is not used in the project

--- SafeZoneController.cs
+++ SafeZoneController.cs
@@ -16,12 +16,21 @@ public class SafeZoneController : MonoBehaviour {
 	public GameObject evolveButton;
 	public GameObject catchButton;
 
-	public GameObject jumpEvolve;
-	public GameObject shieldEvolve;
-	public GameObject shootEvolve;
+	public Button jumpButton;
+	public Button speedButton;
+	public Button shieldButton;
+
+	public Text jumpEvolve;
+	public Text speedEvolve;
+	public Text shieldEvolve;
+
+	private int scoreHolder;
+	private float healthHolder;
 
 	// Use this for initialization
 	void Start () {
+		//line for texting to be removed
+		Stats.score = 140;
 		//Show the most recent data on the player stats
 		UpdateScore ();
 		UpdateCoins ();
@@ -30,7 +39,16 @@ public class SafeZoneController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//if since last frame score has been updated (e.g. because of collision with falling objects), call UpdateScore function and put current scroe into the score holder for future checks
+		if (scoreHolder != Stats.score) {
+			UpdateScore ();
+			scoreHolder = Stats.score;
+		}
+		//check if the health value has been changed as well
+		if (healthHolder != Stats.health) {
+			UpdateHealth();
+			healthHolder = Stats.health;
+		}
 	}
 
 	public void ShowEvolvePanel () {
@@ -64,5 +82,58 @@ public class SafeZoneController : MonoBehaviour {
 
 	void UpdateCoins () {
 		coinsText.text = "Coins:\n" + Stats.coins;
+	}
+
+	public GameObject errorPanel;
+	public GameObject successPanel;
+
+	public void ButtonPressedCallback (int index) {
+		switch (index) {
+		//index 1 = Jump Evolve, costs 20 points
+		case 1:
+			if (Stats.score < 20) {
+				StartCoroutine (ShowErrorMessage ());
+			} else {
+				Stats.score -= 20;
+				StartCoroutine (ShowSuccessMessage ());
+				jumpEvolve.color = Color.gray;
+				jumpButton.interactable = false;
+			}
+			break;
+			//index 2 = Speed Evolve, costs 40 points
+		case 2:
+			if (Stats.score < 40) {
+				StartCoroutine (ShowErrorMessage ());
+			} else {
+				Stats.score -= 40;
+				StartCoroutine (ShowSuccessMessage ());
+				speedEvolve.color = Color.gray;
+				speedButton.interactable = false;
+			}
+			break;
+			//index 3 = Shield Evolve, costs 60 points
+		case 3:
+			if (Stats.score < 60) {
+				StartCoroutine (ShowErrorMessage ());
+			} else {
+				Stats.score -= 60;
+				StartCoroutine (ShowSuccessMessage ());
+				shieldEvolve.color = Color.gray;
+				shieldButton.interactable = false;
+			}
+			break;
+		}
+	}
+
+	private IEnumerator ShowErrorMessage () {
+		errorPanel.SetActive (true);
+		yield return new WaitForSeconds (2.0f);
+		errorPanel.SetActive (false);
+	}
+
+	private IEnumerator ShowSuccessMessage () {
+		successPanel.SetActive (true);
+		yield return new WaitForSeconds (2.0f);
+		successPanel.SetActive (false);
 	}
 }
