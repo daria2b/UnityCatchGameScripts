1fb2a464a1ae80583b10075206b0296deef7e6d3
Daria2b <d.bagina@gmail.com>
2017-08-14 07:19:08 -0400

    Initial commit for floating text
    NOTE: Not in use in the game

--- FloatingText.cs
+++ FloatingText.cs
@@ -5,60 +5,60 @@ using UnityEngine.UI;
 
 //This script will be used to display text over player when he catches falling object or is hit by one
 public class FloatingText : MonoBehaviour {
-	//references to needed game objects
-	public GameObject floatingCanvas;
-	public Text valueText;
-	public Image icon;
+
+	public Text popupText;
+	public Image popupIcon;
+	SpriteRenderer spriteRenderer;
 	
-	public Sprite[] icons;		//icons should go in this order: [0] health, [1] shield, [2] coins, [3] points
-	
-	private float healthHolder;
-	private float shieldHolder;
-	private int coinsHolder;
-	private int scoreHolder;
+//	private float healthHolder;
+//	private float shieldHolder;
+//	private int coinsHolder;
+//	private int scoreHolder;
 	
 	void Start () {
-		floatingCanvas.SetActive (false);
+		spriteRenderer = popupIcon.GetComponent<SpriteRenderer> ();
+		//pass in the amount of time it wait before destroying the object
+		Destroy (gameObject, 1.3f);
+
 		//At start remember the initial values for the player stats that can be affected by the falling object
-		healthHolder = Stats.currentHealth;
-		shieldHolder = Stats.currentShield;
-		coinsHolder = Stats.coins;
-		scoreHolder = Stats.score;
+//		healthHolder = Stats.currentHealth;
+//		shieldHolder = Stats.currentShield;
+//		coinsHolder = Stats.coins;
+//		scoreHolder = Stats.score;
 	}
 	
-	void Update () {
-		// if any of the values has changed, change the text and icon on the floating canvas and initiate the coroutine
-		if (healthHolder != Stats.currentHealth) {
-			valueText.text = "" + (Stats.currentHealth - healthHolder);
-			icon.sprite = icons[0];
-			StartCoroutine (Display ());
-			healthHolder = Stats.currentHealth;
-		}
-		if (shieldHolder != Stats.currentShield) {
-			valueText.text = "" + (Stats.currentShield - shieldHolder);
-			icon.sprite = icons[1];
-			StartCoroutine (Display ());
-			shieldHolder = Stats.currentShield;
-		}
-		if (coinsHolder != Stats.coins) {
-			valueText.text = "" + (Stats.coins - coinsHolder);
-			icon.sprite = icons[2];
-			StartCoroutine (Display ());
-			coinsHolder = Stats.coins;
-		} 
-		if (scoreHolder != Stats.score) {
-			valueText.text = "" + (Stats.score - scoreHolder);
-			icon.sprite = icons[3];
-			StartCoroutine (Display ());
-			scoreHolder = Stats.score;
-		}
-	}
-	
-	IEnumerator Display () {
-		//Display the panel for 1 second then turn it off
-		floatingCanvas.SetActive (true);
-		yield return new WaitForSeconds (1.0f);
-		floatingCanvas.SetActive (false);
+//	void Update () {
+//		// if any of the values has changed, change the text and icon on the floating canvas and initiate the coroutine
+//		if (healthHolder != Stats.currentHealth) {
+//			valueText.text = "" + (Stats.currentHealth - healthHolder);
+//			popupIcon.sprite = icons[0];
+//		
+//			healthHolder = Stats.currentHealth;
+//		}
+//		if (shieldHolder != Stats.currentShield) {
+//			valueText.text = "" + (Stats.currentShield - shieldHolder);
+//			popupIcon.sprite = icons[1];
+//
+//			shieldHolder = Stats.currentShield;
+//		}
+//		if (coinsHolder != Stats.coins) {
+//			valueText.text = "" + (Stats.coins - coinsHolder);
+//			popupIcon.sprite = icons[2];
+//
+//			coinsHolder = Stats.coins;
+//		} 
+//		if (scoreHolder != Stats.score) {
+//			valueText.text = "" + (Stats.score - scoreHolder);
+//			popupIcon.sprite = icons[3];
+//
+//			scoreHolder = Stats.score;
+//		}
+//	}
+
+	public void SetTextIcon (string text, Sprite icon) {
+		popupText.text = text;
+
+		spriteRenderer.sprite = icon;
 	}
 	
 }
---
+++ FloatingTextController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingTextController : MonoBehaviour {
+
+	private static FloatingText popupText;
+	//this will be used for setting correct location
+	private static GameObject canvas;
+
+	//Since we can't use a prefab throught the inspector in this case, we're creating our won initialization method
+	//This method will be responsible to getting a reference to an object or pther parameters we need to set up
+	public static void Initialize () {
+
+		canvas = GameObject.Find ("PopupCanvas");
+		//it's going to get this prefab and it's going to return the type for us as FloatingText
+		//we have a reference to it, but we'll have to initialize it
+		if (!popupText)		//if reference is null, create a reference 
+			popupText = Resources.Load<FloatingText>("Prefabs/PopupTextParent"); 
+
+	}
+
+	public static void CreateFloatingText (string text, Sprite icon, Transform location) {
+		//instance gives a reference to the created object. After that we can access files and components from this reference
+		FloatingText instance = Instantiate (popupText);
+		instance.transform.SetParent (canvas.transform, false);
+		instance.SetTextIcon (text, icon);
+	}
+}
