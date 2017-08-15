cb07efddfa86f6f3cd3fdc33ec468bbc4904eec6
Daria B <d.bagina@gmail.com>
2017-08-15 13:34:56 -0400

    Dummy file for the CS50 presentation video

---
+++ CS50.cs
@@ -0,0 +1,17 @@
+using CS50.Material;
+using CS50.Encouragement;
+using Passion;
+using CommonSense;
+
+public class CS50 : OnePersonProject {	
+	
+	//These variables will be used to define my project
+	public string myName = "Daria Bagina";
+	public string nickname = "Daria2B";
+	public string address = "Toronto, Canada";
+	public string projectTitle = "2D Game Catch Evolve";
+	
+	void Start () {
+		StartGame ();
+	}
+}
