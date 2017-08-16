f061255ca0671ad7c0f17e75198c7d3c71eb767e
Daria2b <d.bagina@gmail.com>
2017-08-15 22:52:17 -0400

    Changed title screen buttons

--- StartButton.cs
+++ StartButton.cs
@@ -6,11 +6,11 @@ using UnityEngine.SceneManagement;
 public class StartButton : MonoBehaviour {
 
 	//references to the game objects we want to interact with when starting the game
-	public GameObject startButton;
+	public GameObject buttonsPanel;
 
 	//this will act with the Start button to start the game
 	public void StartGame () {
-		startButton.SetActive (false);
+		buttonsPanel.SetActive (false);
 		//start spawning objects once the start button is clicked
 		SceneManager.LoadScene (2);
 	}
