f4370992e238b3573d9b9387f37e7cbf86749d88
Daria2b <d.bagina@gmail.com>
2017-08-19 09:25:15 -0400

    Resets player on ground boolean
    This script is not used in game

---
+++ Grounding.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounding : MonoBehaviour {
+
+	//reference to the script attached to the player
+	public PlayerController playerController;
+
+	void OnTriggerEnter2D (Collider2D other) {
+		Debug.Log ("Collided");
+		if (other.gameObject.tag == "Player") {
+			playerController.onGround = true;
+		}
+	}
+		
+}
