ff1d3bcdd45a8af57ba3861e8655d951996dcc9b
Daria2b <d.bagina@gmail.com>
2017-08-19 14:39:15 -0400

    New scripts for Game Over panel

--- GoToMenu.cs
+++ GoToMenu.cs
@@ -6,6 +6,26 @@ using UnityEngine.SceneManagement;
 public class GoToMenu : MonoBehaviour {
 
 	public void StartGameAnew () {
+		//reset all static value to their initial values to start the game anew
+		Evolution.playerLevel = 1;
+		Evolution.jump = false;
+		Evolution.speed = false;
+		Evolution.shield = false;
+
+		HighScoreScript.highScoreValley = 0;
+		HighScoreScript.highScoreSands = 0;
+		HighScoreScript.highScoreRocks = 0;
+		HighScoreScript.highScoreForest = 0;
+
+		Stats.score = 0;
+		Stats.maxHealth = 50;
+		Stats.currentHealth = 50;
+		Stats.coins = 0;
+		Stats.maxShield = 0;
+		Stats.currentShield = 0;
+
+		Stats.timeBought = 0;
+
 		SceneManager.LoadScene (0);
 	}
 }
